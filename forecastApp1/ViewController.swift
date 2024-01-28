400cea5d0e565d9f0ecf476df3c4243cc2c8f826
Mykaela <mykaela.wallen25@d155.org>
2024-01-28 10:55:20 -0600

    Initial Commit

---
+++ forecastApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  forecastApp1
+//
+//  Created by MYKAELA WALLEN on 1/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
